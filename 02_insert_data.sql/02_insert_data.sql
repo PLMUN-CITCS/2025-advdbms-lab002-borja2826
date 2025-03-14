46b4632a016f70419ff93a312fb79d73e48778b9
Edwin <borjaedwinjr_bsit@plmun.edu.ph>
2025-03-15 00:03:31 +0800

    Completed assignment: Created tables and inserted data

---
+++ 02_insert_data.sql/02_insert_data.sql
@@ -0,0 +1,1 @@
+Inserting data into tblStudents INSERT INTO tblStudents (student_name, student_email, student_phone) VALUES ('John Doe', 'johndoe@gmail.com', '123-456-7890'), ('Jane Doe', 'janedoe@gmail.com', '123-456-7890');
